2ebced9b6d3d481a59b207ada8865a187cdb8d44
MarcilioDevOps <155991969+marciliossalgado@users.noreply.github.com>
2024-12-17 14:51:06 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const apiKey = '77c4e2b070a2e1396500d0b42ebf7cec'; // chave api
+let currentPage = 1;
+let totalPages = 1;
 
 const searchInput = document.getElementById('search-input');
+const movieList = document.getElementById('movie-list');
+const previousButton = document.getElementById('previous-btn');
+const nextButton = document.getElementById('next-btn');
 
 searchInput.addEventListener('input', function() {
   const query = searchInput.value.trim();
   if (query.length > 2) { // Começa a pesquisa quando o usuário digitar mais de 2 caracteres
-    const searchUrl = `https://api.themoviedb.org/3/movie/popular?api_key=77c4e2b070a2e1396500d0b42ebf7cec&language=pt-BR`;
-
-    fetch(searchUrl)
-      .then(response => response.json())
-      .then(data => {
-        displayMovies(data.results);
-      })
-      .catch(error => console.error('Erro ao buscar filmes:', error));
+    currentPage = 1; // Reseta a página quando uma nova pesquisa é feita
+    fetchMovies(query);
   }
 });
 
+function fetchMovies(query = '') {
+  const searchUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=${currentPage}&query=${query}`;
+
+  fetch(searchUrl)
+    .then(response => response.json())
+    .then(data => {
+      displayMovies(data.results);
+      totalPages = data.total_pages;
+      updatePagination();
+    })
+    .catch(error => console.error('Erro ao buscar filmes:', error));
+}
+
 function displayMovies(movies) {
-  const movieList = document.getElementById('movie-list');
   movieList.innerHTML = ''; // Limpa a lista antes de adicionar novos filmes
 
   movies.forEach(movie => {
@@ -31,15 +42,31 @@ function displayMovies(movies) {
       <img src="${posterUrl}" alt="${movie.title}">
       <h3>${movie.title}</h3>
       <p>${movie.release_date}</p>
-      <button onclick="showMovieDetails(${movie.id})">Detalhes</button>
+      <p>${movie.overview}</p>
     `;
 
     movieList.appendChild(movieItem);
   });
 }
 
-// Definição da função showMovieDetails
-function showMovieDetails(movieId) {
-  console.log(`Mostrar detalhes do filme com ID: ${movieId}`);
-  // Aqui você pode adicionar a lógica para exibir detalhes do filme
+function updatePagination() {
+  previousButton.disabled = currentPage === 1;
+  nextButton.disabled = currentPage === totalPages;
 }
+
+previousButton.addEventListener('click', function() {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchMovies(searchInput.value.trim());
+  }
+});
+
+nextButton.addEventListener('click', function() {
+  if (currentPage < totalPages) {
+    currentPage++;
+    fetchMovies(searchInput.value.trim());
+  }
+});
+
+// Inicializa a primeira pesquisa para popular filmes
+fetchMovies();
